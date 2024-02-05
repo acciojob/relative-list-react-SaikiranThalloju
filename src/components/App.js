410baa1ca72582fd728deb5a48a44a3dfef737b9
Saikiran Thalloju <saikiranthalloju008@gmail.com>
2024-02-05 20:05:30 +0530

    checked

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
+  // Define the array of relatives
+  const relatives = [
+    'Gajala',
+    'Padmanabha Simha',
+    'Padmasri',
+    'McDonald Murthy'
+  ];
+
   return (
     <div id="main">
-               {/* Do not remove the main div */}
-               <ol key={relatives}>
-                   <li key={relativeListItem1}>Gajala</li>
-                   <li key={relativeListItem2}>Padmanabha Simha</li>
-                   <li key={relativeListItem3}>Padmasri</li>
-                   <li key={relativeListItem4}>mc donald murthy</li>
-               </ol>
+      {/* Do not remove the main div */}
+      <ol key="relativeList">
+        {/* Map through the relatives array and generate list items */}
+        {relatives.map((relative, index) => (
+          <li key={`relativeListItem${index + 1}`}>{relative}</li>
+        ))}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
