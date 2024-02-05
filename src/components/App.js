68d6f0c332601cd9b23b80b15cb3803d96fda04c
Saikiran Thalloju <saikiranthalloju008@gmail.com>
2024-02-05 20:00:05 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,12 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol key={relatives}>
+                   <li key={relativeListItem1}>Gajala</li>
+                   <li key={relativeListItem2}>Padmanabha Simha</li>
+                   <li key={relativeListItem3}>Padmasri</li>
+                   <li key={relativeListItem4}>mc donald murthy</li>
+               </ol>
     </div>
   )
 }
