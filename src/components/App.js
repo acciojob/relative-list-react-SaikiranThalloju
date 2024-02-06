b425fafe03f9e5f645d578eb86f1985ba228e9e4
Saikiran Thalloju <saikiranthalloju008@gmail.com>
2024-02-06 15:11:40 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -12,11 +12,11 @@ const App = () => {
   return (
     <div id="main">
       {/* Do not remove the main div */}
-      <ol key="relativeList">
+      <ol id= "relativeList" key = "relativeList">
         {/* Map through the relatives array and generate list items */}
        
         {relatives.map((relative, index) => (
-          <li key={`relativeListItem${index + 1}`}>{relative}</li>
+          <li id = {`relativeListItem${index + 1}`} key={`relativeListItem${index + 1}`}>{relative}</li>
         ))}
       </ol>
     </div>
